5d4f058f099bfaeda976d15207eab9f402a2fced
Tom Smallwood <tomwildcat9@gmail.com>
2021-11-15 08:55:48 -0700

    added review functionality

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -41,6 +41,8 @@ app.component('product-display', {
 
       </div>
     </div>
+    <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+    <review-form @review-submitted="addReview"></review-form>
   </div>`,
   data() {
     return {
@@ -51,7 +53,8 @@ app.component('product-display', {
         variants: [
           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
           { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-        ]
+        ],
+        reviews: []
     }
   },
   methods: {
@@ -77,6 +80,9 @@ app.component('product-display', {
           return 'Free'
         }
         return 2.99
+      },
+      addReview(review) {
+        this.reviews.push(review)
       }
   }
 })
